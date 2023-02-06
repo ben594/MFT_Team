9943acf0ee4d7f9752a81010819fc790238d9efb
ben594 <benli20042013@gmail.com>
2023-02-06 18:17:59 -0500

    Implemented trader using weighted average of returns

--- intro_project/trader_class.py
+++ intro_project/trader_class.py
@@ -1,3 +1,6 @@
+import math
+import pandas as pd
+
 class Trader:
     def __init__(self):
         # Add any additional info you want
@@ -37,4 +40,47 @@ class SampleTrader(Trader):
                 trades['Stock2'] = 1000
             else:
                 trades['Stock2'] = -1000
+        return trades
+
+
+class BenTrader(Trader):
+    def __init__(self):    
+        self.weighted_avg_return = {"Stock1": 0, "Stock2": 0, "Stock3": 0, "Stock4": 0}
+        self.history = {"Stock1": [1], "Stock2": [1], "Stock3": [1], "Stock4": [1]} 
+        self.num_to_name = ["Stock1", "Stock2", "Stock3", "Stock4"]
+        self.predicted_return = {"Stock1": 0, "Stock2": 0, "Stock3": 0, "Stock4": 0}
+    
+    def MakeTrades(self, time, stock_prices):
+        trades = {}
+
+        for i in range(4): 
+            # currently at time t, but only know prices up to time t-1
+            # estimate return from t-1 to t
+            name = self.num_to_name[i]
+            prev_price = stock_prices[name] # price at t-1
+            prev_avg_return = self.weighted_avg_return[name] # average return until t-2
+            prev_return = (prev_price - self.history[name][-1]) / self.history[name][-1] # return from t-2 to t-1
+            self.history[name].append(prev_price) # add t-1 price to history
+            if time < 10:
+                cur_return = prev_return
+            else:
+                cur_return = prev_avg_return * 0.75 + prev_return * 0.25 # use average return until t-1 as return estimate from t-1 to t
+            self.weighted_avg_return[name] = cur_return
+            self.predicted_return[name] = cur_return
+            
+            quantity = 1
+            if time >= 10:
+                if math.fabs(prev_return) < math.fabs(prev_avg_return):
+                    quantity = math.fabs(prev_return) / math.fabs(prev_avg_return) * 10
+                else:
+                    quantity = math.fabs(prev_return) / math.fabs(prev_avg_return) * 200
+                quantity = int(quantity)
+            
+            quantity = min(quantity, 10000)
+            
+            # assume estimated return from t to t+1 is same as estimated return from t-1 to t
+            if cur_return > 0:
+                trades[name] = quantity
+            else:
+                trades[name] = -1 * quantity
         return trades
